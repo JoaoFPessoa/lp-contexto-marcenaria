5c48b8b40239466f5212bf55c849d3685669c627
JoaoFPessoa <joaoppessoa@uol.com.br>
2024-08-07 19:44:37 -0300

    add: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import { Josefin_Sans } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const josefinSans = Josefin_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Contexto Marcenaria",
   description: "",
+  category: "marcenaria",
 };
 
 export default function RootLayout({
@@ -16,6 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <body className={josefinSans.className}>{children}</body>
     </html>
   );
