a44c91cb99b8404dcf4968df238d0daad95daeaf
JoaoFPessoa <joaoppessoa@uol.com.br>
2024-08-07 19:45:14 -0300

    add component: header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,33 +1,36 @@
 import Image from "next/image";
-import Logo from "../../../static/Logo-Preto.png";
+import Logo from "../../../public/Header/Logo-Branco.png";
 import Link from "next/link";
 
 export default function Header() {
   return (
-    <div className="absolute  top-0 w-full flex justify-between items-center max-w-screen-2xl  bg-transparent  border border-black">
-      <Image src={Logo} width={100} alt="contexto-logo" />
+    <div className="absolute top-0 w-full flex justify-between items-center px-[10%] bg-transparent  ">
+      <div className="absolute inset-0 bg-black opacity-20"></div>
+      <Link href={"/"}>
+        <Image src={Logo} width={80} alt="contexto-logo" />
+      </Link>
 
-      <nav className="flex gap-4 text-xl">
+      <nav className="flex gap-24 text-3xl  font-semibold text-white">
         <Link
-          className="transition-transform duration-300 hover:scale-110"
+          className="underline-animation transition-transform duration-300 hover:scale-110"
           href="/"
         >
           Início
         </Link>
         <Link
-          className="transition-transform duration-300 hover:scale-110"
+          className="underline-animation transition-transform duration-300 hover:scale-110"
           href="/projects"
         >
           Projetos
         </Link>
         <Link
-          className="transition-transform duration-300 hover:scale-110"
+          className="underline-animation transition-transform duration-300 hover:scale-110"
           href="/about"
         >
           Sobre
         </Link>
         <Link
-          className="transition-transform duration-300 hover:scale-110"
+          className="underline-animation transition-transform duration-300 hover:scale-110"
           href="/"
         >
           Arquitetos
