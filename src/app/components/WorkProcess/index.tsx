2cdcd8c3743643efde664e3c561b3eb9f52e4f93
JoaoFPessoa <joaoppessoa@uol.com.br>
2024-08-24 16:47:39 -0300

    fix: improved cards logic

--- src/app/components/WorkProcess/index.tsx
+++ src/app/components/WorkProcess/index.tsx
@@ -1,68 +1,52 @@
+"use client";
 import WorkProgressCard from "./Card";
 
 export default function WorkProgress() {
+  const cards = [
+    {
+      title: "Contato",
+      description:
+        "será um prazer participar da realização do seu sonho! nesta etapa vamos entender suas necessidades e desejos.",
+    },
+    {
+      title: "Projeto",
+      description:
+        "nós trabalhamos com projetos de arquitetos e designers, mas também executamos projetos próprios...",
+    },
+    {
+      title: "Proposta",
+      description:
+        "neste etapa nós assinamos contrato, efetuamos pagamento e daremos início na produção...",
+    },
+    {
+      title: "Medição",
+      description:
+        "para iniciarmos a produção da sua marcenaria, precisamos confirmar todos os detalhes...",
+    },
+    {
+      title: "Produção",
+      description:
+        "nesta etapa nós estamos preparando seu projeto. trocas de informações e dúvidas podem aparecer...",
+    },
+    {
+      title: "Instalação | Finalização",
+      description:
+        "nesta etapa nós estamos preparando seu projeto. trocas de informações e dúvidas podem aparecer...",
+    },
+  ];
+
   return (
-    <div className="flex flex-col px-[10%] py-28 bg-slate-950">
-      <h1 className="text-white text-7xl mb-16">Processo de trabalho</h1>
-      <div className="flex-col lg:flex-row flex flex-wrap justify-between gap-12">
-        <WorkProgressCard
-          title={"Contato"}
-          description="será um prazer participar da realização
-do seu sonho!
-nesta etapa vamos entender suas
-necessidades e desejos.
-"
-        />
-        <WorkProgressCard
-          title={"Projeto"}
-          description="nós trabalhamos com projetos de
-arquitetos e designers, mas também
-executamos projetos próprios.
-caso não tenha, nós enviaremos uma
-prévia de orçamento baseado em
-medidas, referências e materiais.
-projetos próprios e executivos são
-desenvolvidos após a aprovação da
-proposta.
-"
-        />
-        <WorkProgressCard
-          title={"Proposta"}
-          description="neste etapa nós assinamos contrato,
-efetuamos pagamento e daremos
-início na produção.
-o prazo será definido em acordo com
-a produção e cliente.
-nesta etapa os projetos são iniciados.
-
-"
-        />
-        <WorkProgressCard
-          title={"Medição"}
-          description="para iniciarmos a produção da sua
-marcenaria, precisamos confirmar todos
-os detalhes do projeto e a medida para
-execução na obra.
-
-"
-        />
-        <WorkProgressCard
-          title={"Produção"}
-          description="nesta etapa nós estamos preparando seu
-projeto. trocas de informações e dúvidas
-podem aparecer. teremos contato direto
-para tudo ser alinhado.
-
-"
-        />
-        <WorkProgressCard
-          title={"Instalação | Finalização"}
-          description="nesta etapa nós estamos preparando seu
-projeto. trocas de informações e dúvidas
-podem aparecer. teremos contato direto
-para tudo ser alinhado.
-"
-        />
+    <div className="flex flex-col  w-full max-w-screen-2xl  items-stretch  p-28">
+      <h1 className="text-primary text-7xl mb-16">Processo de trabalho</h1>
+      <div className="flex flex-col w-full justify-between items-stretch">
+        {cards.map((card, idx) => (
+          <WorkProgressCard
+            key={idx}
+            index={(idx + 1).toString().padStart(2, "0")} // Create an index like "01", "02"
+            title={card.title}
+            description={card.description}
+          />
+        ))}
       </div>
     </div>
   );
